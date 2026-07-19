1a11a4b7509111f3c10b9e387555c5397943f726
Haru1113-cloud <Haru1113-cloud@users.noreply.github.com>
2026-07-19 01:51:20 +0000

    deploy: 48a0e0906f3984c32cdfcba3b8f19f2cfbf86bdf

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "433114888" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2269300204" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
